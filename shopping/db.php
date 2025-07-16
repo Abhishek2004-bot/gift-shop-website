3f3c6008d4d5c663237f8079941ce303598913f8
Abhishek S L <abhisheksl081@gmail.com>
2025-07-16 23:22:42 +0530

    Update db.php

--- shopping/db.php
+++ shopping/db.php
@@ -4,5 +4,4 @@ $mysql_username = "root";
 $mysql_password = "";
 $server_name = "localhost:3306";
 $conn = mysqli_connect($server_name,$mysql_username,$mysql_password,$db_name);
- echo "s";
-  ?>
+ ?>
